83c9eb192b379e2fc7b09bee5491fe612696c0cf
Daan Bernelot Moens <daanmoens@hotmail.com>
2022-02-14 12:58:45 +0100

    committing challenge 2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+# comment
+class QuestionsController < ApplicationController
+  def ask
+    # empty
+  end
+
+  def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+
+    if params[:question] == 'I am going to work'
+      @answers = @answers[0]
+    elsif params[:question].split('')[-1] == '?'
+      @answers = @answers[1]
+    else
+      @answers = @answers[2]
+    end
+  end
+end
